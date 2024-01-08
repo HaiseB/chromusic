907c60b6d232b5b0b75b55896dff4427865bca61
Benjamin HAISE <benjaminhaise@gmail.com>
2024-01-08 16:39:14 +0100

    g pas ton temps bro

--- dawholePage/audio.js
+++ dawholePage/audio.js
@@ -1,18 +1,3 @@
-const audioPlayer = document.getElementById('audioPlayer');
-
-audioPlayer.addEventListener('play', function () {
-  // Logique à effectuer lors de la lecture (si nécessaire)
-});
-
-audioPlayer.addEventListener('pause', function () {
-  // Logique à effectuer lors de la pause (si nécessaire)
-});
-
-audioPlayer.addEventListener('ended', function () {
-  // Logique à effectuer lorsque la lecture est terminée (si nécessaire)
-});
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const settingsButton = document.getElementById('settingsButton');
     const additionalParams = document.getElementById('additionalParams');
