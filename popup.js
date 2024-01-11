dfbf39399cc489b76c944fbb10004184efc1f769
Benjamin HAISE <benjaminhaise@gmail.com>
2024-01-11 14:13:10 +0100

    feat : implement direct link

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const openAudioButton = document.getElementById("openAudioPage");
+  const listenRadioPhenixButton = document.getElementById("listenRadioPhenix");
 
   function updateButtonAndRect() {
     // Chercher tous les onglets ouverts
@@ -11,18 +12,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
       // Mettre à jour le texte du bouton en fonction de l'existence de l'onglet
       openAudioButton.textContent = audioPageOpen
-        ? "Go to Audio Player"
-        : "Open Audio Player";
+        ? "Go to Audio page"
+        : "Open Audio Player web page";
 
       // Mettre à jour la classe du div "playing" en fonction de l'existence de l'onglet
       playingRect.classList.toggle("playing", audioPageOpen);
     });
   }
 
+  listenRadioPhenixButton.addEventListener("click", function () {
+    // Ouvrir une nouvelle page dans un nouvel onglet
+    chrome.tabs.create({
+        url: chrome.runtime.getURL("webPage/index.html?playing=RadioPhenix"),
+    }, function (newTab) {
+        // Récupérer l'onglet actuel
+        chrome.tabs.getCurrent(function (currentTab) {
+            // Revenir à l'onglet actuel après un délai (par exemple, 1000 ms)
+            setTimeout(function () {
+                chrome.tabs.update(currentTab.id, { active: true });
+            }, 1000);
+        });
+    });
+});
+
+
   openAudioButton.addEventListener("click", function () {
     // Chercher tous les onglets ouverts
     chrome.tabs.query({}, function (tabs) {
-      // Vérifier si l'onglet "audio.html" est déjà ouvert
+      // Vérifier si l'onglet "index.html" est déjà ouvert
       const audioPageOpen = tabs.some(
         (tab) => tab.url && tab.url.includes("webPage/index.html")
       );
--- webPage/js/phenixRadio.js
+++ webPage/js/phenixRadio.js
@@ -1,21 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const playPhenixAsked = window.location.href.includes('playing=RadioPhenix');
     const phenixRadioButton = document.getElementById('phenixRadioPlayPauseButton');
     const audioElement = new Audio('https://live.radio-campus.org:8002/caen.mp3');
     const pageTitleElement = document.querySelector('.modal-title');
     let isPlaying = false;
 
+    function updateUI() {
+        phenixRadioButton.innerHTML = `<span class="fade-in"><i class="fas fa-${isPlaying ? 'pause' : 'play'} fa-fw"></i></span>&nbsp; Radio Phénix`;
+        pageTitleElement.innerHTML = `Chromusic <span class="fade-in">${isPlaying ? '- Radio Phénix is playing' : ''}</span>`;
+        document.title = `Chromusic${isPlaying ? ' - Radio Phénix is playing' : ''}`;
+    }
+
+    if (playPhenixAsked) {
+        isPlaying = true;
+        audioElement.play();
+        updateUI();
+    }
+
     phenixRadioButton.addEventListener('click', function () {
         if (isPlaying) {
             audioElement.pause();
-            phenixRadioButton.innerHTML = '<span class="fade-in"><i class="fas fa-play fa-fw"></i></span>&nbsp; Radio Phénix';
-            pageTitleElement.textContent = 'Chromusic';
-            document.title = 'Chromusic';
         } else {
             audioElement.play();
-            phenixRadioButton.innerHTML = '<span class="fade-in"><i class="fas fa-pause fa-fw"></i></span>&nbsp; Radio Phénix';
-            pageTitleElement.innerHTML = 'Chromusic <span class="fade-in">- Radio Phénix is playing</span>';
-            document.title = 'Chromusic - Radio Phénix is playing';
         }
         isPlaying = !isPlaying;
+        updateUI();
     });
 });
