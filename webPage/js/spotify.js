9d57951992ae960c658f9a861e573fcebf89aef3
Benjamin HAISE <benjaminhaise@gmail.com>
2024-01-18 17:26:07 +0100

    feat(spotify) WIP : add form to choose other playlists

--- webPage/js/spotify.js
+++ webPage/js/spotify.js
@@ -1,3 +1,12 @@
+async function getPlaylists() {
+    return new Promise((resolve) => {
+        chrome.storage.local.get(["spotifyPlaylists"], function (result) {
+            resolve(JSON.stringify(result.spotifyPlaylists, null, 2));
+            console.log("Playlists retrieved : ", JSON.stringify(result.spotifyPlaylists, null, 2));
+        });
+    });
+}
+
 function clickRandomButton() {
     // ToDo find the right selector
     const spotifyButtons = document.querySelectorAll('[id="buttonTertiary"]');
@@ -8,12 +17,65 @@ function clickRandomButton() {
 
         randomButton.click();
     } else {
-        console.log('impossible to play a random playlist');
+        console.log("impossible to play a random playlist");
     }
 }
 
-document.addEventListener('DOMContentLoaded', function () {
-    if (window.location.href.includes('playing=true')) {
+async function start() {
+    const playlists = await getPlaylists();
+    const playlistTextarea = document.getElementById("playlistTextarea");
+
+    if (!playlists) {
+        const defaultSpotifyPlaylists = {
+            playlists: [
+                {
+                    title: "Spotify Embed: Recommendation Playlist",
+                    src: "https://open.spotify.com/embed/playlist/32NkwlZQYqno5HauO6FWvU",
+                },
+                {
+                    title: "Daily mix 1",
+                    src: "https://open.spotify.com/embed/playlist/37i9dQZF1E39VcQRsR3o26",
+                },
+                {
+                    title: "Daily mix 2",
+                    src: "https://open.spotify.com/embed/playlist/37i9dQZF1E36miGJAIINge",
+                },
+            ],
+            albums: [
+                {
+                    title: "Ashnikko - Demidevil",
+                    src: "https://open.spotify.com/embed/album/438ToDoVaJH5aTIXXrlDyI",
+                },
+                {
+                    title: "Kalika - Adieu les monstres",
+                    src: "https://open.spotify.com/embed/album/0UrbIea2sXlvZfMhD9PkFA",
+                },
+                {
+                    title: "Melanie Martinez - Portals",
+                    src: "https://open.spotify.com/embed/album/4kI7ZZF6CgDGFTjZNFwXYG",
+                },
+            ],
+        };
+
+        chrome.storage.local.set(
+            { spotifyPlaylists: defaultSpotifyPlaylists },
+            function () {
+                console.log("Playlists saved : ", defaultSpotifyPlaylists);
+            }
+        );
+
+        playlistTextarea.value = defaultSpotifyPlaylists;
+    } else {
+        playlistTextarea.value = playlists;
+    }
+}
+
+start();
+
+document.addEventListener("DOMContentLoaded", function () {
+    const playlistTextarea = document.getElementById("playlistTextarea");
+
+    if (window.location.href.includes("playing=true")) {
         clickRandomButton();
     }
 });
