9fe3136b554d5d19354c191d7b6b87b72d580003
Benjamin HAISE <benjaminhaise@gmail.com>
2024-01-18 09:20:39 +0100

    fix(themeSelection) : when you launch the extension without avec selected a theme

--- popup.js
+++ popup.js
@@ -2,12 +2,13 @@ async function getSelectedTheme() {
   return new Promise((resolve) => {
       chrome.storage.local.get(['selectedTheme'], function (result) {
           resolve(result.selectedTheme);
+          console.log("Theme retrieved : "+result.selectedTheme)
       });
   });
 }
 
 async function start() {
-  const selectedTheme = await getSelectedTheme();
+  const selectedTheme = (await getSelectedTheme()) || 'vapor';
   const cssFileName = `${selectedTheme}.bootstrap.min.css`;
   const cssFilePath = `./bootstrap/${cssFileName}`;
 
